f2c281a545d68ef1ed32ca15686b3d83c59b51c4
ksfallon <fallon.katelin@gmail.com>
2021-06-25 13:28:08 -0500

    added router.get and router.post in category-routes.js for the  'find all categories' and 'create a new category' but need to add a section to include associated Products for both

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500),json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id);
+    if(!categoryData) {
+      res
+    }
+  }
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
+  // Category.create({
+  //   category_name: req.body.category_name
+  // })
+  // .then((newCategory) => {
+  //   res.json(newCategory);
+  // })
+  // .catch((err) => {
+  //   res.json(err);
+  // })
 });
 
 router.put('/:id', (req, res) => {
