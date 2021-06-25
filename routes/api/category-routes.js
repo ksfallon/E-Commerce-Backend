9e9c3d37e67164b73d4612e295446ef538088615
ksfallon <fallon.katelin@gmail.com>
2021-06-25 13:58:44 -0500

    added associated Products to my router.get s in category-routes.js with include model: Product within my const categoryData

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,19 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+       // be sure to include its associated Products
+      include: [{model: Product}]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500),json(err);
   }
-  // be sure to include its associated Products
+ 
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
-    const categoryData = await Category.findByPk(req.params.id);
-    if(!categoryData) {
+    const categoryData = await Category.findAll({
+      // be sure to include its associated Products
+      include: [{model: Product}]
+    });
+      if(!categoryData) {
       res.status(404).json({ message: 'No user with this id!' });
       return;
     }
