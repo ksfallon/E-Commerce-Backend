f14456f86ad51ddb26552af1361281e474777f71
ksfallon <fallon.katelin@gmail.com>
2021-06-25 14:31:14 -0500

    added router.get (/) 'find all products' and router.get(/:id) 'find a single product by id'

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -12,9 +12,9 @@ router.get('/', async (req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500),json(err);
+    res.status(500).json(err);
   }
- 
+
 });
 
 router.get('/:id', async (req, res) => {
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
       include: [{model: Product}]
     });
       if(!categoryData) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(categoryData);
@@ -63,7 +63,7 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!categoryData[0]) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(categoryData);
@@ -81,7 +81,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No user with this id!' });
+      res.status(404).json({ message: 'No category with this id!' });
       return;
     }
     res.status(200).json(categoryData);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,12 +6,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+       // be sure to include its associated Category and Tag data
+      include: [{model: Category}, {model: Tag}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ 
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
+  try {
+    const productData = await Product.findAll({
+      // be sure to include its associated Category and Tag data
+     include: [{model: Category}, {model: Tag}]
+    });
+      if(!productData) {
+      res.status(404).json({ message: 'No product with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
