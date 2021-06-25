48d608e1db64f4f4543f6c91c6ab150a91593ed7
ksfallon <fallon.katelin@gmail.com>
2021-06-25 09:40:58 -0500

    finished  fields for product.js

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -19,18 +19,32 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    price: { 
-      type: DataTypes.DECIMAL(10,2),
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       isDecimal: true,
-    }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
