3fd774f937b19796693869567b2b45b1135ffde8
ksfallon <fallon.katelin@gmail.com>
2021-06-25 09:54:25 -0500

    added foreign key connections in index.js

--- models/index.js
+++ models/index.js
@@ -5,6 +5,9 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 
 // Categories have many Products
 Category.hasMany(Product, {
@@ -12,8 +15,15 @@ Category.hasMany(Product, {
 })
 
 // Products belongToMany Tags (through ProductTag)
-
+ProductTag.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id"
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id"
+})
 
 module.exports = {
   Product,
