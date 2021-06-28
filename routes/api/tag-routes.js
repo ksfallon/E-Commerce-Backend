ed5f7ca4599dddc5b0aca5b9a50cacb569454c07
ksfallon <fallon.katelin@gmail.com>
2021-06-28 11:12:23 -0500

    updated tag-routes const to reflect get, post, put and delete CRUD methods. I will have a diff CREATE functnio that product-routes and see if they both work or not. Also forgot to include req.params.id for the ID fetching and req.body in the destroy

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,11 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all tags
   try {
-    const tagData = await Tag.findAll({
+    const allTagData = await Tag.findAll({
        // be sure to include its associated Products
-      include: [{model: Product}]
+      include: [{model: Product}] //Should it be ProductTag bc thats how tag and product are connected?
     });
-    res.status(200).json(tagData);
+    res.status(200).json(allTagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -21,7 +21,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Tag.findByPk({
+    const tagData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Products
       include: [{model: Product}]
     });
@@ -39,8 +39,9 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create(req.body);
-    res.status(200).json(tagData);
+    const newTagData = await Tag.create(req.body);
+    res.status(200).json(newTagData);
+    //going to try it this way vs the other way that I do in product routes to see if there is a difference.
   } catch (err) {
     res.status(400).json(err)
   }
@@ -49,16 +50,16 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(req.body, {
+    const updateTagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    if (!tagData[0]) {
+    if (!updateTagData[0]) {
       res.status(404).json({ message: 'No tag with this id!' });
       return;
     }
-    res.status(200).json(tagData);
+    res.status(200).json(updateTagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -67,16 +68,16 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.destroy({
+    const deleteTagData = await Tag.destroy(req.body, {
       where: {
         id: req.params.id
       },
     });
-    if (!tagData) {
+    if (!deleteTagData) {
       res.status(404).json({ message: 'No tag with this id!' });
       return;
     }
-    res.status(200).json(tagData);
+    res.status(200).json(deleteTagData);
   } catch (err) {
     res.status(500).json(err);
   }
