2d40f8895b9556682f1825e99ae0874190b6c81b
ksfallon <fallon.katelin@gmail.com>
2021-06-28 22:02:30 -0500

    removing foreign keys from index.js in models table to see if that helps with the errors in insomnia

--- models/index.js
+++ models/index.js
@@ -5,24 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: "category_id"
-})
+Product.belongsTo(Category
+//   {
+//   foreignKey: "category_id"
+// }
+)
 
 // Categories have many Products
-Category.hasMany(Product, {
-  foreignKey: "category_id"
-})
+Category.hasMany(Product 
+//   {
+//   foreignKey: "category_id"
+// }
+)
 
 // Products belongToMany Tags (through ProductTag)
 ProductTag.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: "product_id"
+  through: ProductTag
+  // foreignKey: "product_id"
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: "tag_id"
+  through: ProductTag
+  // foreignKey: "tag_id"
 })
 
 module.exports = {
