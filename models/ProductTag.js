993c48831bd120150bcba943bae64e0ebceed131
ksfallon <fallon.katelin@gmail.com>
2021-06-29 00:07:57 -0500

    removed through:product tag - not sure why the joins aren't happy with my products and tags, trying to figure this out

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -16,15 +16,15 @@ ProductTag.init(
       type: DataTypes.STRING,
       references: {
         model: "product",
-      key: "id"
-    },
+        key: "id"
+      },
     },  
     tag_id: {
       type: DataTypes.INTEGER,
       references: {
         model: "tag",
-      key: "id"
-    },
+        key: "id"
+      },
     }, 
   },
   {
